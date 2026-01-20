a6db13d6647106e95f617ac4dda830078e99ebd5
mayconfpds <mayconfpds@gmail.com>
2026-01-20 16:19:46 -0300

    Update app.py

--- app.py
+++ app.py
@@ -25,7 +25,7 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 # --- CONFIGURAÇÃO DE EMAIL (Reconfigure aqui após instalar) ---
 app.config['MAIL_SERVER'] = 'smtp.gmail.com'
-app.config['MAIL_PORT'] = 587
+app.config['MAIL_PORT'] = 465
 app.config['MAIL_USE_TLS'] = True
 app.config['MAIL_USERNAME'] = os.environ.get('MAIL_USERNAME') 
 app.config['MAIL_PASSWORD'] = os.environ.get('MAIL_PASSWORD') 
